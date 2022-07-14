4b6116c13257d93e7c2a1b99303b1114fb41ca49
-rawatsum <rawatsum@gmail.com>
2022-07-14 17:29:51 +0530

    initial commit

---
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Tests;
+
+public class UnitTest1
+{
+    
+    WeatherForecastController controller = new WeatherForecastController();
+        
+         [Fact]
+    public void getTest()
+    {
+        var arr = controller.Get(1);
+        Assert.Matches("sumit",arr.ToString());
+        
+    }
+
+[Fact]
+    public void getTest1()
+    {
+        var arr1 = controller.Get(1,2);
+        Assert.Matches("ravikant",arr1.ToString());
+    }
+
+}
